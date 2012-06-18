910d22a523726fdd9f5995cc5176cb115ab4c5d7
Your Name <manveru0@gmail.com>
2012-06-19 00:04:33 +0200

    update to 3.0.22: http://www.kernel.org/pub/linux/kernel/v3.0/ChangeLog-3.0.22

--- mm/slub.c
+++ mm/slub.c
@@ -1827,6 +1827,11 @@ static void *__slab_alloc(struct kmem_cache *s, gfp_t gfpflags, int node,
 	slab_lock(page);
 	if (unlikely(!node_match(c, node)))
 		goto another_slab;
+	
+	/* must check again c->freelist in case of cpu migration or IRQ */
+	object = c->freelist;
+	if (object)
+		goto update_freelist;
 
 	stat(s, ALLOC_REFILL);
 
@@ -1837,6 +1842,7 @@ load_freelist:
 	if (kmem_cache_debug(s))
 		goto debug;
 
+update_freelist:
 	c->freelist = get_freepointer(s, object);
 	page->inuse = page->objects;
 	page->freelist = NULL;
